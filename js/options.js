05ea35eb7f199302922d8b90abe1a771c04e6218
Joyce Liao <joyliao07@gmail.com>
2018-07-30 13:16:55 -0700

    link js to html for Options page

--- js/options.js
+++ js/options.js
@@ -1,1 +1,28 @@
 'use strict';
+
+var varFood = document.getElementById('food');
+var varHealthInsurance = document.getElementById('healthInsurance');
+var varVehicle = document.getElementById('vehicle');
+var varClothes = document.getElementById('clothes');
+var varHousing = document.getElementById('housing');
+var varSleep = document.getElementById('sleep');
+var varDental = document.getElementById('dental');
+var varSocial = document.getElementById('social');
+
+varFood.addEventListener('click', )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
