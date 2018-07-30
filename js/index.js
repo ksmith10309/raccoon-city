9987b9368fe9b9f0ae1af7d19386cb67f39d18ff
Andrew Fields <andyfieldsy89@gmail.com>
2018-07-30 11:41:43 -0700

    starting username storing javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+
+
+var formEl = document.getElementById('user-form');
+formEl.addEventListener('submit', function(event) {
+  console.log('event.target', event);
+  console.log(event.target.Username.value);
+});
