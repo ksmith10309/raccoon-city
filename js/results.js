2e351f284a1ce57eb8e04e4b888e931fa3183d57
Elijah Bruce <elijahbruce907@gmail.com>
2018-07-31 13:42:37 -0700

    Js result if else statements

--- js/results.js
+++ js/results.js
@@ -14,3 +14,50 @@ var clickSleep = JSON.parse(localStorage.getItem('Sleep'));
 var clickDental = JSON.parse(localStorage.getItem('Dental'));
 var clickSocial = JSON.parse(localStorage.getItem('Social'));
 
+if(clickFood === true) {
+  console.log('true test');
+} else if(clickFood === false) {
+  console.log('false test');
+}
+
+if(clickHealth === true) {
+  console.log('true test');
+} else if(clickHealth === false) {
+  console.log('false test');
+}
+
+if(clickVehicle === true) {
+  console.log('true test');
+} else if(clickVehicle === false) {
+  console.log('false test');
+}
+
+if(clickClothes === true) {
+  console.log('true test');
+} else if(clickClothes === false) {
+  console.log('false test');
+}
+
+if(clickHousing === true) {
+  console.log('true test');
+} else if(clickHousing === false) {
+  console.log('false test');
+}
+
+if(clickSleep === true) {
+  console.log('true test');
+} else if(clickSleep === false) {
+  console.log('false test');
+}
+
+if(clickDental === true) {
+  console.log('true test');
+} else if(clickDental === false) {
+  console.log('true test');
+}
+
+if(clickSocial === true) {
+  console.log('true test');
+} else if(clickSocial === false) {
+  console.log('true test');
+}
